2faa92e6c4151be94a5e51400ac8aa65e8c78846
Jacob McCann <jacob.mccann2@target.com>
2017-01-30 14:38:20 -0600

    Version 0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
-# 0.2.0
+# 0.3.0
+* Version bump `chef-vault` cookbook to `~> 2.1` (from `~> 1.3`)
 
+# 0.2.0
 * Initial Release
 
 # 0.1.0
-
 * Initial Commit
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures a docker host'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/jmccann/docker-host-cookbook'
 issues_url       'https://github.com/jmccann/docker-host-cookbook/issues'
-version          '0.2.0'
+version          '0.3.0'
 
 depends 'chef-vault', '~> 2.1'
 depends 'docker', '~> 2.9'
