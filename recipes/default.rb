e7db91b68bee951f708e43e2b7ad6bd90bb08c68
Jacob McCann <jacob.mccann2@target.com>
2017-05-30 10:49:10 -0500

    Restart docker is storage_driver isn't correct
    Seems docker can be started from package before service files are
    written causing it to not restart properly

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,3 +18,9 @@ docker_service_manager 'default' do
   retries 1
   retry_delay 20
 end
+
+execute 'restart docker' do
+  command 'exit 0'
+  notifies :restart, 'docker_service_manager[default]', :immediate
+  not_if "docker info | grep 'Storage Driver: #{node['jmccann-docker-host']['docker']['service']['storage_driver']}'"
+end
